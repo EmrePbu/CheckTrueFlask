c4e6acf7863ab95253efac9d397b3fa9d2143989
EmrePbu <emrewindowsarg@gmail.com>
2021-01-28 12:09:49 +0300

    added file upload system

--- app.py
+++ app.py
@@ -1,19 +1,63 @@
+import os
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request, send_from_directory
 from flask_mysqldb import MySQL
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from wtforms import Form, StringField, TextAreaField, PasswordField, FileField, validators
 from passlib.hash import sha256_crypt
-import os
+from flask_wtf import FlaskForm
 from werkzeug.utils import secure_filename
 from werkzeug.middleware.shared_data import SharedDataMiddleware
 
+UPLOAD_FOLDER = 'documents'
+ALLOWED_EXTENSIONS = {'doc', 'docx', 'pdf'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 # main page route
-@app.route('/')
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # bu return işlemi kullanıcıya dosya indirmesi için redirect yapmayı sağlıyor
+            # return redirect(url_for('uploaded_file',
+            #                        filename=filename))
     return render_template('index.html')
+
+
+"""
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+
+# istenilen dosyanın kullanıcıya sunulması işlemini yapıyor
+# yani kullanıcının bilgisayarına dosya indirmek için
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
+"""
 
 
 # doc page
