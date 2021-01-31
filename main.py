f2ac499d80485f21ec74d1d43f1c45657d0f0947
EmrePbu <emrewindowsarg@gmail.com>
2021-01-31 18:46:44 +0300

    change py file name

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
 # main page route
+
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
